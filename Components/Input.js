f7902e5d1d303c9cf853fd5a6b6f970b66fcbd29
Axel <qianwangfan@ultraman.local>
2024-09-12 20:15:21 -0700

    create input component and pass the focus state to input element

---
+++ Components/Input.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect, useRef} from 'react';
+import {TextInput, Text, Button, View} from 'react-native';
+
+export default function Input({onFocusChange, autoFocus}) {
+
+
+    return (
+        <View>
+
+        </View>
+    );
+}
