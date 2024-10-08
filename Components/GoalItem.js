c7dde0ad068b317bd15a9a5816527131bb1e6cbd
Axel <qianwangfan@ultraman.local>
2024-10-08 14:59:33 -0700

    Use a pressable prop to give the user an option to delete the goal by doing a long press on the goal item.

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,4 +1,4 @@
-import {Button, Pressable, StyleSheet, Text, View} from "react-native";
+import {Alert, Button, Pressable, StyleSheet, Text, View} from "react-native";
 import React from "react";
 import {useNavigation} from "@react-navigation/native";
 import PressableButton from "./PressableButton";
@@ -17,6 +17,19 @@ const GoalItem = ({goal, handleDelete}) => {
                 onPress={function () {
                     navigation.navigate("Details", {goal: goal});
                 }}
+                onLongPress={() => {
+                    Alert.alert("Delete Goal", "Are you sure you want to delete this goal?", [ {
+                        text: "Yes",
+                        onPress: () => {
+                            handleDelete(goal.id);
+                        },
+                    }, {
+                        text: "No",
+                        onPress: () => console.log("No Pressed"),
+                        style: "cancel",
+                    }, ], {cancelable: true});
+                    }
+                }
             >
                 <Text style={styles.text}>{goal.text}</Text>
 
@@ -24,11 +37,7 @@ const GoalItem = ({goal, handleDelete}) => {
                     customStyle={styles.buttonStyle}
                     onPressHandler={() => {
                         handleDelete(goal.id);
-                    }}
-                    longPressHandler={() => {
-                        console.log("long press");
-                    }}
-                >
+                    }}>
                     {/*Delete Button’s Icon    */}
                     <FontAwesome name="trash" size={24} color="black"/>
                 </PressableButton>
