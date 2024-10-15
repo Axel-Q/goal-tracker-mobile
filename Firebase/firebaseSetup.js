4b199320b68143fbfb9a86842a00ff96e92a0bf7
Axel <qianwangfan@ultraman.local>
2024-10-15 13:32:39 -0700

    move credentials to env file

--- Firebase/firebaseSetup.js
+++ Firebase/firebaseSetup.js
@@ -1,29 +1,15 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
 import {getFirestore} from "firebase/firestore";
-// import {
-//   apiKey,
-//   authDomain,
-//   projectId,
-//   storageBucket,
-//   messagingSenderId,
-//   appId,
-// } from "@env";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    // apiKey: apiKey,
-    // authDomain: authDomain,
-    // projectId: projectId,
-    // storageBucket: storageBucket,
-    // messagingSenderId: messagingSenderId,
-    // appId: appId,
-    apiKey: "AIzaSyA2I19uPHVHBkL_MBHgzHymKtsyZ6uvKfM",
-    authDomain: "mobile-axel.firebaseapp.com",
-    projectId: "mobile-axel",
-    storageBucket: "mobile-axel.appspot.com",
-    messagingSenderId: "34976857120",
-    appId: "1:34976857120:web:d981a0a64f913a43c15045"
+    apiKey: process.env.EXPO_PUBLIC_apiKey,
+    authDomain: process.env.EXPO_PUBLIC_authDomain,
+    projectId: process.env.EXPO_PUBLIC_projectId,
+    storageBucket: process.env.EXPO_PUBLIC_storageBucket,
+    messagingSenderId: process.env.EXPO_PUBLIC_messagingSenderId,
+    appId: process.env.EXPO_PUBLIC_appId,
 };
 
 // Initialize Firebase
