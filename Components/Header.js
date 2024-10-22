7e4d228daf0b248e3fe365b66468f139e1b3d940
Axel <qianwangfan@ultraman.local>
2024-10-22 15:44:56 -0700

    update app.json setting

--- Components/Header.js
+++ Components/Header.js
@@ -3,25 +3,30 @@ import React, {useEffect, useState} from 'react';
 import {
     StyleSheet,
     Text,
-    useWindowDimensions,
+    useWindowDimensions, View,Dimensions,
 } from "react-native";
 
+
+
 export default function Header({name}) {
-    const {height} = useWindowDimensions();
-    console.log("Current height:", height);
+    const {width,height} = useWindowDimensions();
+    console.log("Current width:", width);
 
     return (
-        <Text
-            style={[
-                styles.text,
-                {
-                    paddingVertical: height < 415 ? 0 : 10,
-                    paddingHorizontal: height < 415 ? 0 : 10,
-                },
-            ]}
-        >
-            Welcome to {name}
-        </Text>
+        <View>
+            <Text
+                style={[
+                    styles.text,
+                    {
+                        paddingVertical: height < 415 ? 0 : 10,
+                        paddingHorizontal: height < 415 ? 0 : 10,
+                        color: height < 415 ? "red": "darkmagenta",
+                    },
+                ]}
+            >
+                Welcome to {name}
+            </Text>
+        </View>
     );
 }
 
@@ -31,7 +36,6 @@ const styles = StyleSheet.create({
         fontSize: 25,
         borderColor: "darkmagenta",
         borderWidth: 2,
-        padding: 5,
         marginBottom: 10,
     },
 });
